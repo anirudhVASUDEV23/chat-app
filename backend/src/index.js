ba022bbb60b1cc963136b77bc384a7b77b3e2d93
anirudhVASUDEV23 <anirudh.vasudev.kesana@gmail.com>
2025-05-09 22:25:01 +0530

    getting ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,7 @@ import { connectDB } from "./lib/db.js"; // Adjust the path as necessary
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { app, server } from "./lib/socket.js";
+import path from "path";
 
 connectDB();
 
@@ -22,9 +23,17 @@ app.use(express.urlencoded({ extended: true, limit: "20mb" }));
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
+const __dirname = path.resolve();
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
