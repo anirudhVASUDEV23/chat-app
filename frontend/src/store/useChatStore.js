7d26ab0db8b6e1474f69800abf657610a8b13feb
anirudhVASUDEV23 <anirudh.vasudev.kesana@gmail.com>
2025-05-09 22:52:04 +0530

    corrected index.js /* error

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -25,7 +25,7 @@ export const useChatStore = create((set, get) => ({
   getMessages: async (id) => {
     set({ isMessagesLoading: true });
     try {
-      const res = await axiosInstance.get(`/messages/user/${id}`);
+      const res = await axiosInstance.get(`/messages/${id}`);
       set({ messages: res.data });
     } catch (error) {
       toast.error(error.response.data.message);
